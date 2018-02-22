c5661a801571c44a020a2c56a04f65ac9622bc13
Falon Darville <falondarville@gmail.com>
2018-02-21 20:12:44 -0800

    add append button function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,50 @@
-$(document).ready() {
+$(document).ready(function() {
 
-var topics = {"pandas", "turtles", "cats", "lions"};
+var topics = ["pandas", "turtles", "cats", "lions"];
 
-//use a loop to append buttons with each of the topics
+$("#addSearch").on("click", function(event) {
+	event.preventDefault();
+
+	var newTopic = $("#user-input").val().trim();
+
+	topics.push(newTopic);
+
+	appendButton();
+});
+
+//append buttons
+function appendButton() {
+
+	$("#buttons").empty();
+
+	for(var y = 0; y < topics.length; y++) {
+	$("#buttons").append("<button class='btn'>" + topics[y] + "</button>");
+	};
+};
 
 //when user clicks on button, they get 10 static images from giphy
 
-//each static image includes rating (G, PG, ...)
-
 //add a form that takes value from user and adds it to topics array, then shows button
 
-//giphy ajax call
-	//parameters: q, limit, rating
-	//switch protocols from http to https
-	//giphy api key 8bvrXfOVZnPW3TMYuEFO8jRewu4AXh3U
 
-var queryURL = "api.giphy.com/api_key:8bvrXfOVZnPW3TMYuEFO8jRewu4AXh3U&q=" + topics[i] + "&limit=10&rating=g";
+// for(var x = 0; x < topics.length; x++) {
 
-$.ajax {
-	url: queryURL,
-	method: "GET"
-}
+// 	console.log(topics[x]);
 
-};
+// 	//giphy ajax call
+// 	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics[x] + "&api_key=8bvrXfOVZnPW3TMYuEFO8jRewu4AXh3U&limit=10&rating=g";
+
+// 	$.ajax({
+// 		url: queryURL,
+// 		method: "GET"
+// 	}).then(function(response) {
+
+// 		// $("#giphy").append
+// 	});
+// };
+
+appendButton();
+
+});
+
+
