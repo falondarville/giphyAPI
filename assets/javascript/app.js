99761daac262a34cc010470556ca2a69cad77a2e
Falon Darville <falondarville@gmail.com>
2018-02-21 20:22:38 -0800

    display function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,30 +18,25 @@ function appendButton() {
 	$("#buttons").empty();
 
 	for(var y = 0; y < topics.length; y++) {
-	$("#buttons").append("<button class='btn'>" + topics[y] + "</button>");
+	$("#buttons").append("<button class='btn' data-name=" + topics[y] + ">" + topics[y] + "</button>");
 	};
 };
 
 //when user clicks on button, they get 10 static images from giphy
+function displayGiphys() {
 
-//add a form that takes value from user and adds it to topics array, then shows button
+	var topic = $(this).attr("data-name");
 
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics[x] + "&api_key=8bvrXfOVZnPW3TMYuEFO8jRewu4AXh3U&limit=10&rating=g";
 
-// for(var x = 0; x < topics.length; x++) {
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	}).then(function(response) {
 
-// 	console.log(topics[x]);
-
-// 	//giphy ajax call
-// 	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics[x] + "&api_key=8bvrXfOVZnPW3TMYuEFO8jRewu4AXh3U&limit=10&rating=g";
-
-// 	$.ajax({
-// 		url: queryURL,
-// 		method: "GET"
-// 	}).then(function(response) {
-
-// 		// $("#giphy").append
-// 	});
-// };
+		// $("#giphy").append
+	});
+};
 
 appendButton();
 
